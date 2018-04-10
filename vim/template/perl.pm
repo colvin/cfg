779fd27462bbe42fd65925f09cff9c6c302528f3
colvin <cwellborn@riscnetworks.com>
2018-04-10 10:19:42 -0400

    vim: templates
    - add perl template files to `~/.vim/template/`
    - move the `ale_linters` definition for perl to after/ftplugin

---
+++ vim/template/perl.pm
@@ -0,0 +1,21 @@
+package REPLACE::ME;
+
+use strict;
+use Data::Dumper;
+
+$Data::Dumper::Sortkeys	= 1;
+$Data::Dumper::Terse	= 1;
+
+1;
+
+__END__
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
